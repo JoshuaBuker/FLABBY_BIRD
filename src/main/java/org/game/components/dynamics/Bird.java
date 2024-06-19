b2ca2060c914ee62b8f7b5b1dd2018e5239072a8
Joshua Buker <joshbuker04@gmail.com>
2024-06-18 23:14:11 -0700

    Tested bird as percent dim - Failed

--- src/main/java/org/game/components/dynamics/Bird.java
+++ src/main/java/org/game/components/dynamics/Bird.java
@@ -4,6 +4,7 @@ import org.lib.color.ColorRGBA;
 import org.lib.component.DynamicComponent;
 import org.lib.draw.Quad;
 import org.lib.listener.KeyListener;
+import org.lib.positioning.PercentDimension;
 import org.lib.positioning.PercentPoint;
 import org.lib.positioning.PixelDimension;
 import org.lwjgl.glfw.GLFW;
@@ -12,7 +13,7 @@ public class Bird extends DynamicComponent {
   private boolean previousButtonState = false;
 
   public Bird() {
-    this.setDimensions(new PixelDimension(50, 50));
+    this.setDimensions(new PercentDimension(0.05f, 0.05f));
     this.setLocation(new PercentPoint(0.0f, 0.0f));
     this.setColor(ColorRGBA.YELLOW);
   }
@@ -20,9 +21,9 @@ public class Bird extends DynamicComponent {
   @Override
   public void applyPhysics() {
     PercentPoint temp = this.getPercentLocation();
-    temp.setY(temp.getY() + 0.1f);
+    temp.setY(temp.getY() + 0.0001f);
 
-    // this.setLocation(temp);
+    this.setLocation(temp);
   } 
 
   @Override
